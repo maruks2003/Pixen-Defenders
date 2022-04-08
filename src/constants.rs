b777f7769f29f5a2b6ac076ef06ded726ef9960c
relativehysteria <rruziro@gmail.com>
2022-04-09 00:48:58 +0200

    Window settings

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,10 @@
+//! Constants that can be tuned to globally change various things.
+//!
+//! These shouldn't be exposed to the user.
 use bevy::prelude::*;
 
+/// The background color used when the scene is cleared
 pub const CLEAR_COLOR: Color = Color::rgb(0., 0., 0.);
+
+/// Window title (of the game)
+pub const GAME_TITLE: &str = "Pixen defenders";
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,23 @@ use pixen_defenders::{
 
 fn main() {
     App::new()
+        // Set the clear background color
         .insert_resource(ClearColor(CLEAR_COLOR))
+
+        // Window settings
+        // XXX: Fix these
+        //   * Fixed resolution
+        //   * Vsync always on
+        // TODO: Add some more settings
+        .insert_resource(WindowDescriptor {
+            width:     1920.,
+            height:    1080.,
+            title:     GAME_TITLE.to_string(),
+            vsync:     true,
+            resizable: false,
+            ..Default::default()
+        })
+
         .add_plugins(DefaultPlugins)
         .run();
 }
